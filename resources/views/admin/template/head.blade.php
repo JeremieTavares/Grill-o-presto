9895856a6195a1d02ab47d9c449f2fd9ac4e5e5f
Jeremie Tavares <jeremietavares@hotmail.fr>
2022-08-25 17:22:27 -0400

    rename des fichiers

--- resources/views/admin/template/head.blade.php
+++ resources/views/admin/template/head.blade.php
@@ -4,6 +4,6 @@
     <title>Title</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    @vite(['resources/scss/app.scss',  'resources/js/app.js'])
+    @vite(['resources/scss/import.scss',  'resources/js/app.js'])
   </head>
   <body>
