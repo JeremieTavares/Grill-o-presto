fc87c59df2ca7c4613483b29d3d43d7d4531c336
all0o <zacharyblais3@gmail.com>
2022-08-26 22:56:10 -0400

    commencement des migration
    Migrations pour la partie repas

--- resources/views/public/accueil.blade.php
+++ resources/views/public/accueil.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 
-<h1>Bienvenue sur la vue du projet</h1>
+
     
 @endsection
---
+++ resources/views/public/template/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar bg-light fixed-top navbar-expand-lg px-5 py-3">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img width="80" src="./image/logo.svg" alt="logo"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-end flex-grow-1 pe-5">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Support</a>
+            </li>
+          </ul>
+          <div class="d-flex gap-3 align-items-center">
+            <a href="" class=""><i class="fa-solid fa-cart-shopping fs-5 me-3"></i></a>
+            <a class="btn btn-primary">Connexion</a>
+            <a class="btn btn-secondary">Inscription <i class="fa fa-arrow-right"></i></a>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <i class="fa-solid fa-cart-shopping"></i>
