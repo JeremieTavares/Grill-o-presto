729fcb7e4b5243744b9f887eccc390ebb2f0bb31
i <isaac.viens@gmail.com>
2022-08-31 14:10:06 -0400

    mise a jour layout bootstrap

--- resources/views/public/confirm.blade.php
+++ resources/views/public/confirm.blade.php
@@ -1,40 +1,42 @@
 @extends('public.template.base')
 
 @section('content')
-    <div class="confirmed">
-        <div class="blocHeader">
-            <h1> Merci (para) pour votre commande</h1>
-        </div>
+    <div class="top">
         <div>
+            <h2 class="container d-flex justify-content-center"> Merci (para)</h2>
+            <h2 class="container d-flex justify-content-center">D'avoir commander!</h2>
+        </div>
+        <div class="container d-flex justify-content-center">
             <img src="" alt="Image de ok confirm">
         </div>
         <div>
-            <p>N# de commande:(para)</p>
-            <p>Prix: (para)</p>
-            <p>Client: (para)</p>
+            <p  class="container d-flex justify-content-center">N# de commande:(para)</p>
+            <p  class="container d-flex justify-content-center">Prix: (para)</p>
+            <p  class="container d-flex justify-content-center">Client: (para)</p>
         </div>
-        <div class="confirmed">
+        <div class="confirmed container d-flex justify-content-center">
             <a href="" class="btn btn-primary">Retour à la page d'accueil</a>
         </div>
     </div>
 
 
-    <div class="refused">
-        <div class="blocHeader">
+    <div class="refused ">
+        <div class="container d-flex justify-content-center">
             <h1> Erreur lors de la commande</h1>
         </div>
-        <div>
+        <div class="container d-flex justify-content-center">
             <img src="" alt="Image de X pas confirm">
         </div>
         <div>
-            <p>N# de commande:(para)</p>
-            <p>Prix: (para)</p>
-            <p>Client: (para)</p>
-            <p>Raison: (para)</p>
+            <p class="container d-flex justify-content-center">N# de commande:(para)</p>
+            <p class="container d-flex justify-content-center">Prix: (para)</p>
+            <p class="container d-flex justify-content-center">Client: (para)</p>
+            <p class="container d-flex justify-content-center">Raison: (para)</p>
         </div>
-        <div class="refused">
+        <div class="refused container d-flex justify-content-center p-2">
             <a href="" class="btn btn-primary">Réessayer</a>
             <a href="" class="btn btn-primary">Support client</a>
         </div>
     </div>
 @endsection
+
