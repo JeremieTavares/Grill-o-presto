fd0c792bda027066793ad00e444a45efd674734f
Jeremie Tavares <84427506+JeremieTavares@users.noreply.github.com>
2022-09-02 17:31:00 -0700

    Creation MODEL - MIGRATION - CONTROLLER
    creation des model avec leurs relations entre eux
    
    migration
    
    et controller avec les functions necessaire mais pas encore remplis

---
+++ database/migrations/2022_09_01_230926_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use App\Models\Ticket_type;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Ticket_type::class)->constrained();
+            $table->text('description');
+            $table->foreignIdFor(User::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
