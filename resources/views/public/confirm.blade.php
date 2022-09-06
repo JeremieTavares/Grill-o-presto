569163b9d77e6ff9723f121d584c7dd83eb292c8
i <isaac.viens@gmail.com>
2022-09-06 15:54:21 -0400

    modif scss
    faire marcher le scss sur cette branche

--- resources/views/public/confirm.blade.php
+++ resources/views/public/confirm.blade.php
@@ -1,13 +1,14 @@
 @extends('public.template.base')
 
 @section('content')
+<main class="confirmation">
     <div class="confirmed">
         <div>
             <h2 class="container d-flex justify-content-center"> Merci {{-- {{ $order->orderClient}} --}}</h2>
             <h2 class="container d-flex justify-content-center">D'avoir commander!</h2>
         </div>
         <div class="container d-flex justify-content-center">
-            <img src="{{URL ('images/confirmLogo.png')}}" class="img-fluid" alt="Image de ok confirm">
+            <img src="{{URL ('images/confirmLogo.png')}}" class="confirmImg" alt="Image de ok confirm">
         </div>
         <div>
             <p  class="container d-flex justify-content-center">N# de commande:{{-- {{ $order->orderNumber}} --}}</p>
@@ -19,5 +20,7 @@
             <a href="" class="btn btn-primary">Retour à la page d'accueil</a>
         </div>
     </div>
+</main>
+    
 @endsection
 
--- resources/views/public/denied.blade.php
+++ resources/views/public/denied.blade.php
@@ -1,22 +1,24 @@
 @extends('public.template.base')
 
 @section('content')
-<div class="refused ">
-    <div class="container d-flex justify-content-center">
-        <h1> Erreur lors de la commande</h1>
+    <div class="refused ">
+        <main class="confirmation">
+            <div class="container d-flex justify-content-center">
+                <h1> Erreur lors de la commande</h1>
+            </div>
+            <div class="container d-flex justify-content-center">
+                <img src="{{ URL('images/deniedLogo.png') }}" class="confirmImg" alt="Image de X pas confirm">
+            </div>
+            <div>
+                <p class="container d-flex justify-content-center">N# de commande:{{-- {{ $order->orderNumber}} --}}</p>
+                <p class="container d-flex justify-content-center">Prix: {{-- {{ $order->orderPrice}} --}}</p>
+                <p class="container d-flex justify-content-center">Client: {{-- {{ $order->orderClient}} --}}</p>
+                <p class="container d-flex justify-content-center">Raison: (para)</p>
+            </div>
+            <div class="refused container d-flex justify-content-center p-2">
+                <a href="" class="btn btn-primary">Réessayer</a>
+                <a href="" class="btn btn-primary">Support client</a>
+            </div>
     </div>
-    <div class="container d-flex justify-content-center">
-        <img src="{{URL ('images/deniedLogo.png')}}" class="img-fluid" alt="Image de X pas confirm">
-    </div>
-    <div>
-        <p class="container d-flex justify-content-center">N# de commande:{{-- {{ $order->orderNumber}} --}}</p>
-        <p class="container d-flex justify-content-center">Prix: {{-- {{ $order->orderPrice}} --}}</p>
-        <p class="container d-flex justify-content-center">Client: {{-- {{ $order->orderClient}} --}}</p>
-        <p class="container d-flex justify-content-center">Raison: (para)</p>
-    </div>
-    <div class="refused container d-flex justify-content-center p-2">
-        <a href="" class="btn btn-primary">Réessayer</a>
-        <a href="" class="btn btn-primary">Support client</a>
-    </div>
-</div>
+    </main>
 @endsection
