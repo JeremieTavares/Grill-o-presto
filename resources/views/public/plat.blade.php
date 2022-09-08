fab5b735fa75c7225694e5d0d52e3a68f5cba32a
i <isaac.viens@gmail.com>
2022-09-08 19:59:58 -0400

    Upload pour merge manquant pour benoit

--- resources/views/public/plat.blade.php
+++ resources/views/public/plat.blade.php
@@ -1,14 +1,11 @@
 @extends('public.template.base')
+@section('banner-title', '{{-- {{ $meal->mealName}} --}} Poulet au saumon')
 
 @section('content')
-    <main class="plat">
-        <div class="container d-flex justify-content-center">
-            <h1>Le plat sélectionné{{-- {{ $meal->mealName}} --}}</h1>
-        </div>
-
-        <div class="d-flex justify-content-center p-5">
+    <main class="plat d-flex flex-column px-3 my-5">
+        <div class="d-flex justify-content-center cardRepas mb-4">
             <div class="card mb-3">
-                <img src="{{URL ('images/meatDish.jpg')}}" class="card-img-top" alt="image de viande"> 
+                <img src="{{ URL('images/cajun.webp') }}" class="card-img-top" alt="image de viande">
                 <div class="card-body">
                     <h2 class="card-title display-6">Parametre nom{{-- {{ $meal->mealNumber}} --}}</h2>
                     <p class="card-text">Parametre categorie{{-- {{ $meal->mealDescription}} --}}</p>
@@ -16,52 +13,54 @@
             </div>
         </div>
         <div class="container">
-            <div class="d-flex justify-content-left p-4 pl-5 align-items-center">
-                <h3 class="m-0">Plât:</h3>
+            <div class="d-flex justify-content-left align-items-center">
+                <h4 class="m-0">Plât:</h4>
                 <p class="m-0">Saumon Roti( Faux plat) {{-- {{ $meal->mealName} --}}</p>
             </div>
-            <div class="d-flex justify-content-left p-4 align-items-center">
-                <h3 class="m-0">Menu:</h3>
+            <div class="d-flex justify-content-left align-items-center my-4">
+                <h4 class="m-0">Menu:</h4>
                 <p class="m-0">Vegetarien (faux Menu){{-- {{ $meal->mealMenu}} --}}</p>
             </div>
-            <div class="d-flex justify-content-left p-4 align-items-center">
+            <div class="d-flex justify-content-left align-items-center">
                 <h4>Description du plat:{{-- {{ $meal->mealMenu}} --}}</h4>
             </div>
         </div>
-      
 
 
-        <div class="container pt-5 pb-5">
-            <div class="d-flex justify-content-center p-1">
-                <h3>Allergènes</h3>
+
+        <div class="container d-flex  my-5 align-items-center">
+            <div class="d-flex justify-content-center">
+                <h4>Allergènes</h4>
             </div>
-           <div class="d-flex justify-content-center">
-            <a href="#" class="p-2"><img src="{{URL ('images/milkAllergen.png')}}" alt="du lait"></a>
-            <a href="#" class="p-2"><img src="{{URL ('images/wheatAllergen.png')}}" alt="du ble"></a>
-           </div>
-            
+            <div class="d-flex justify-content-center">
+                <a href="#" class="p-2"><img class="allergenImg" src="{{ URL('images/milkAllergen.png') }}"
+                        alt="du lait"></a>
+                <a href="#" class="p-2"><img class="allergenImg" src="{{ URL('images/wheatAllergen.png') }}"
+                        alt="du ble"></a>
+            </div>
+
         </div>
 
 
         <div class="container d-flex justify-content-center">
-            <h3>Choisir la portion</h3>
+            <h4>Choisir la portion</h4>
         </div>
 
         <div class="container d-flex justify-content-center">
             <a href="" class="btn btn-primary border-0  m-1">1 personne</a>
             <a href="" class="btn btn-primary border-0 m-1">2 personnes</a>
-            <a href="" class="btn btn-primary border-0  m-1">4 Familliale</a>    
+            <a href="" class="btn btn-primary border-0  m-1">4 Familliale</a>
         </div>
 
-        <hr class="breakLine">
-        <div class="container d-flex justify-content-center p-5">
-            <h2>Prix du repas:</h2>
-        </div>
-        <div class="d-flex justify-content-center">
-            <h3>55${{-- {{ $meal->mealPrice}} --}}</h3>
-        </div>
+        <hr class="breakLine w-50 my-4 m-auto">
 
-        <div class="container d-flex justify-content-center p-5">
+        <div class="container d-flex justify-content-center gap-2">
+            <h4>Prix du repas:</h4>
+            <h4>55${{-- {{ $meal->mealPrice}} --}}</h4>
+        </div>
+        
+
+        <div class="container d-flex justify-content-center my-3">
             <a href="" class="btn btn-primary">Ajouter au panier</a>
         </div>
     </main>
--- resources/views/public/template/base.blade.php
+++ resources/views/public/template/base.blade.php
@@ -1,5 +1,10 @@
 @include('public.template.head')
 @include('public.template.navbar')
+
+<div class="w-100 banner p-4 d-sm-none d-md-block">
+    <h2 class="text-center text-black fw-bold"> @yield('banner-title')</h2>
+</div>
+
 @yield('content')
 
 @include('public.template.footer')
