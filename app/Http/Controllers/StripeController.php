5ecf7052017236a2b1e8b5ac2253895b7654fdbb
Xavier B <beauchampx@outlook.com>
2022-09-18 16:58:08 -0400

    Avancement dans Stripe
    J'ai poursuivi l'implémentation de Stripe.

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Stripe;
-use Illuminate\Http\Request;
 use Session;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StripeController extends Controller
 {
@@ -25,18 +27,82 @@ class StripeController extends Controller
      */
     public function stripePost(Request $request)
     {
-        dd($request);
+        // Find existing user
 
+        // $stripeCust =  $stripe->customers->search([
+        //     'query' => 'email:"\beauchampx@outlook.com"',
+        // ]);
+        // $userToken =  $stripe->customers->retrieve(
+        //    'cus_MSM12CcDpafOSl',
+        // );
+        //
+        //dd($request->stripeToken);
         Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
-        Stripe\Charge::create([
-            "amount" => (200) + (200 * 0.15),
-            "currency" => "CAD",
-            "source" => $request->stripeToken,
-            "description" => "Mon nom"
-        ]);
+        $stripe = new \Stripe\StripeClient(
+            'sk_test_51LiLVXLQyEBjABClty9cNuhsOxMMyxAOdg2fAM9SXjI2nwMarcOuVtILIuzEe89E9iF5PmSpwwKhcTZ7N63v2A9800IGFHhOAb'
+        );
 
+
+        $fullName = $request->prenom . ' ' . $request->nom;
+        if (Auth::check()) {
+            $user = User::getLoggedUserInfo()->first();
+            // If the existing user have an StipeToken , it means it already exist
+            // Then we connect that token to the actual stripe transaction (Stripe server side)
+            if ($user->stripeToken > 1) {
+                Stripe\Charge::create([
+                    "amount" => (200) + (200 * 0.15),
+                    "currency" => "CAD",
+                    "customer" => $user->stripeToken,
+                    "description" => "Paiement Gill-O-Presto"
+                ]);
+            } else {
+                // Create new client for Stripe via Logged user
+                $newClientLogged =  $stripe->customers->create([
+                    'description' => "Client email: $user->email, Form email: $request->email",
+                    'name' => $user->prenom . " " . $user->nom,
+                    'email' => $user->email
+                ]);
+                //Update payment card
+                $stripe->customers->createSource(
+                    $newClientLogged->id,
+                    ['source' => $request->stripeToken]
+                );
+                // Create new transaction
+                $paiement = Stripe\Charge::create([
+                    "amount" => (200) + (200 * 0.15),
+                    "currency" => "CAD",
+                    "customer" => $newClientLogged->id,
+                    "description" => "Paiement Gill-O-Presto"
+                ]);
+
+                $user->stripeToken = $newClientLogged->id;
+                $user->save();
+            }
+        } else {
+            $newClient =  $stripe->customers->create([
+                'description' => "Form email: $request->email",
+                'name' => $fullName,
+                'email' => $request->email
+            ]);
+            $stripe->customers->createSource(
+                $newClient->id,
+                ['source' => $request->stripeToken]
+            );
+            // Create new transaction
+            $paiement = Stripe\Charge::create([
+                "amount" => (2002) + (200 * 0.15),
+                "currency" => "CAD",
+                "customer" => $newClient->id,
+                "description" => "Paiement Gill-O-Presto"
+            ]);
+        }
         Session::flash('success', 'Payment successful!');
 
         return back();
     }
 }
+
+
+
+//'customer' =>  'cus_MSMFPr2kPILU55',
+//"source" => $request->stripeToken,
