77e99ab754687c10de7c5ad1fa1095d77e85ec3a
Jeremie Tavares <84427506+JeremieTavares@users.noreply.github.com>
2022-09-23 14:49:06 -0700

    added faq navbar

--- resources/views/admin/gestionFaq/faq-index.blade.php
+++ resources/views/admin/gestionFaq/faq-index.blade.php
@@ -1,6 +1,9 @@
 @extends('admin.template.base')
 @section('banner-title', 'FAQ - Gestion de la FAQ')
 @section('content')
+@if (Auth::user()->role->role === "Admin_1")
+@include('admin.template.sub-navbar-admin-2')
+@endif
 @if (Auth::user()->role->role === "Admin_2")
 @include('admin.template.sub-navbar-admin-2')
 @endif
